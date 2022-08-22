efeee867f928ee0ebdd8fa0b8ca5e503cb79b6a3
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-08-22 18:11:34 -0400

    Confirm Health

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,4 +1,19 @@
 const confirmHealth = (snack) => {
+        let {protein , fiber , added_sugar} = snack;
+        if (typeof protein !== "number" || typeof fiber !== 'number' || typeof added_sugar !== 'number') {
+        return null    
+       } else {
+           if(added_sugar < 5){
+             if(protein >= 5 || fiber >=5){
+                 return true
+             } else {
+               return false
+             }
+         } else {
+             return false
+         }
+       }
+       
 
     
 };
